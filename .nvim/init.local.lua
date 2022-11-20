749af04910bbcc93ed110c7157055cc326eda847
petrosAth <develop@athanasoulis.me>
2022-11-20 10:59:40 +0200

    Update Neovim local config

--- .nvim/init.local.lua
+++ .nvim/init.local.lua
@@ -1,4 +1,6 @@
-_G.user.load_local_config(vim.fn.getcwd(), {
-    use_palettes  = true,
-    use_spellfile = false,
+USER.load_local_config(vim.fn.getcwd(), {
+    use_session        = false,
+    use_spellfile      = false,
+    use_palettes       = true,
+    use_format_on_save = true,
 })

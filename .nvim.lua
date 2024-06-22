9a869a541079d2318889db9984fb25ee47d41013
petrosAth <develop@athanasoulis.me>
2024-06-22 12:00:04 +0300

    Update for ccc and .nvim.lua

---
+++ .nvim.lua
@@ -0,0 +1,7 @@
+USER.load_local_config({
+	use_session = false, -- Use local session
+	use_spellfile = false, -- Use local spell file
+	use_palettes = true, -- Use local palettes
+	use_prettier = true, -- Copy .prettierrc from templates folder
+	use_format_on_save = true, -- Enable LSP format on save
+})
--- .nvim/init.local.lua
+++
@@ -1,7 +0,0 @@
-USER.load_local_config({
-    use_session        = false, -- Use local session
-    use_spellfile      = false, -- Use local spell file
-    use_vale           = false, -- Use Vale linter(with local spell file)
-    use_palettes       = true,  -- Use local hexokinase palettes
-    use_format_on_save = true,  -- Enable LSP format on save
-})
